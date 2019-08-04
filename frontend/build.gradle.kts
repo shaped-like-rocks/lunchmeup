d35c3eb6824751da2894d7b3c9bb0ba2ccb03611
Christian Dräger <christian.draeger1@gmail.com>
2019-08-04 23:00:36 +0200

    frontend: add tasks to bundle and test the react app

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -6,3 +6,16 @@ node {
     version = "12.7.0"
     yarnVersion = "1.17.3"
 }
+
+tasks {
+
+    register("bundle") {
+        description = "bundle react app to browser understandable vanilla js"
+        dependsOn("yarn_bundle")
+    }
+
+    register("check") {
+        description = "Start all frontend tests"
+        dependsOn("yarn_test")
+    }
+}
