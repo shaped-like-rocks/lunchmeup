f57c48832ca881af09e46d1af36bf33ce4106277
Christian Dräger <christian.draeger1@gmail.com>
2019-08-04 22:57:19 +0200

    remove css bundle import from static mainpage template since we use a css-in-js solution to style elements

--- backend/src/main/kotlin/rocks/like/shaped/lunchmeup/MainPageController.kt
+++ backend/src/main/kotlin/rocks/like/shaped/lunchmeup/MainPageController.kt
@@ -25,7 +25,6 @@ class MainPageController {
                 head {
                     title { +"LunchMeUp!" }
                     meta(charset = "utf-8")
-                    link(rel = "stylesheet", type = "text/css", href = "/dist/bundle.css") {}
                 }
 
                 body {
